34b61ab84364f120858070b4af7d5cbf083fe574
Kobi Shmueli <kobi@macron.co.il>
2015-03-23 03:37:29 +0200

    Add delete-karnaf-db.php: a script to delete all karnaf's tickets.

---
+++ php/karnaf-scripts/delete-karnaf-db.php
@@ -0,0 +1,20 @@
+<?
+##################################################################
+# Karnaf HelpDesk System - Copyright (C) 2001-2015 Kobi Shmueli. #
+# See the LICENSE file for more information.                     #
+##################################################################
+/* This is a script to delete the Karnaf database */
+
+require("../ktools.php");
+
+squery("DELETE FROM karnaf_tickets");
+squery("DELETE FROM karnaf_replies");
+squery("DELETE FROM karnaf_actions");
+squery("ALTER TABLE karnaf_tickets AUTO_INCREMENT = 0");
+squery("ALTER TABLE karnaf_replies AUTO_INCREMENT = 0");
+squery("ALTER TABLE karnaf_actions AUTO_INCREMENT = 0");
+
+echo "Done.";
+
+require_once("../contentpage_ftr.php");
+?>
