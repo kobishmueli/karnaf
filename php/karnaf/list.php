7b3baf35805c2d44ae74ce3677212590dac856b7
Kobi Shmueli <kobi@macron.co.il>
2016-04-10 16:57:31 +0300

    Added filter support to list.php.

--- php/karnaf/list.php
+++ php/karnaf/list.php
@@ -11,6 +11,8 @@ make_menus("Karnaf (HelpDesk)");
 if(isset($_GET['status'])) $status = $_GET['status'];
 else $status = 1;
 if(!$status) safe_die("Invalid status!");
+if(isset($_GET['filter'])) $filter = $_GET['filter'];
+else $filter = 0;
 ?>
 <script>
 var time = new Date().getTime();
@@ -28,7 +30,22 @@ function refresh() {
 setTimeout(refresh, 10000);
 </script>
 <form name="form1" id="form1" method="get">
+<select name="filter" onChange="form1.submit();">
+<option value="">---</option>
+<?
+$query2 = squery("SELECT id,name FROM karnaf_filters ORDER BY priority");
+while($result2 = sql_fetch_array($query2)) {
+?>
+<option value="<?=$result2['id']?>"<? if($filter == $result2['id']) echo " SELECTED"; ?>><?=$result2['name']?></option>
+<?
+}
+sql_free_result($query2);
+?>
+</select>
 <select name="status" onChange="form1.submit();">
+<? if($filter != 0) { ?>
+<option value="" SELECTED>---</option>
+<? } ?>
 <option value="999"<? if($status == 999) echo " SELECTED"; ?>>Opened - All non-closed tickets</option>
 <?
 $query2 = squery("SELECT status_id,status_name FROM karnaf_statuses WHERE status_id!=0 AND status_id!=5 ORDER BY status_id");
@@ -80,7 +97,12 @@ INNER JOIN karnaf_cat1 AS c1 ON c1.id=c2.parent INNER JOIN karnaf_statuses AS s 
 up.priority_id=t.upriority INNER JOIN karnaf_priorities AS sp ON
 sp.priority_id=t.priority) WHERE";
 $argv = array();
-if($status == 999) {
+if($filter != 0) {
+  $query2 = squery("SELECT id,name,querystr FROM karnaf_filters WHERE id=%d", $filter);
+  if($result2 = sql_fetch_array($query2)) $qstr .= $result2['querystr'];
+  sql_free_result($query2);
+}
+else if($status == 999) {
   $qstr .= " (t.status!=0 and t.status!=5)";
 }
 else {
