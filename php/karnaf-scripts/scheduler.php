baa641a60f855be2115a19cbbcbe503b3cef4a0d
Kobi Shmueli <kobi@macron.co.il>
2016-07-24 19:26:09 +0300

    Added scheduler_post_actions() support to scheduler.php.

--- php/karnaf-scripts/scheduler.php
+++ php/karnaf-scripts/scheduler.php
@@ -133,5 +133,8 @@ while($result = sql_fetch_array($query)) {
 }
 sql_free_result($query);
 
+/* Run custom post actions code if the scheduler_post_actions() function exists: */
+if(function_exists("scheduler_post_actions")) scheduler_post_actions();
+
 require_once("../contentpage_ftr.php");
 ?>
