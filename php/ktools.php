f5ff9f4c990b5760bfc804241e7f81d82eb5cbb6
Kobi Shmueli <kobi@macron.co.il>
2016-08-26 01:08:01 +0300

    Updated KARNAF_VERSION to 5.0.17.

--- php/ktools.php
+++ php/ktools.php
@@ -7,7 +7,7 @@
 
 require_once("ktools-custom.php");
 require_once("defines.php");
-define("KARNAF_VERSION", "5.0.16");
+define("KARNAF_VERSION", "5.0.17");
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 set_magic_quotes_runtime(0);
 if(!isset($override_magicquotes) && get_magic_quotes_gpc() == 1) die("Error: Incorrect magic_quotes_gpc setting!");
