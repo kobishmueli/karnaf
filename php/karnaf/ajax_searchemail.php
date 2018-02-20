1ebf9d81770ed2969b297722b1f008a798b303a7
Kobi Shmueli <kobi@macron.co.il>
2018-02-20 03:20:05 +0200

    Added ajax_searchemail.php for the reply/replies autocomplete.

---
+++ php/karnaf/ajax_searchemail.php
@@ -0,0 +1,22 @@
+<?php
+##################################################################
+# Karnaf HelpDesk System - Copyright (C) 2001-2018 Kobi Shmueli. #
+# See the LICENSE file for more information.                     #
+##################################################################
+
+require_once("../ktools.php");
+check_auth();
+CheckOperSession();
+
+if(isset($_GET['term'])) {
+  $term = "%".$_GET['term']."%";
+  $query = squery("SELECT user,email,fullname FROM users WHERE user LIKE '%s' OR email LIKE '%s' OR fullname LIKE '%s'",
+                  $term, $term, $term);
+  while(($result = sql_fetch_array($query))) {
+    $results[] = array('id' => $result['id'], "label" => $result['fullname']." - ".$result['email'], "value" => $result['email']);
+  }
+  sql_free_result($query);
+}
+echo json_encode($results);
+require_once("karnaf_footer.php");
+?>
