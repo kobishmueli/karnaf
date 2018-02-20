8d8d6dcaf3b16792804ba193da73c6c3d4729a31
Kobi Shmueli <kobi@macron.co.il>
2018-02-20 03:21:09 +0200

    Added Nir A's patch for reply/replies autocomplete.

--- php/karnaf/edit.php
+++ php/karnaf/edit.php
@@ -512,6 +512,54 @@ function submit3_onclick() {
   document.form1.submit();
 }
 
+    function autocomplate_me(e) {
+        function split(val) {
+            return val.split(/,\s*/);
+        }
+
+        function extractLast(term) {
+            return split(term).pop();
+        }
+
+        $(e.self.event.target).on("keydown", function (event) {
+            if (event.keyCode === $.ui.keyCode.TAB &&
+                $(e.self.event.target).autocomplete("instance").menu.active) {
+                event.preventDefault();
+            }
+        })
+            .autocomplete({
+                source: function (request, response) {
+                    $.getJSON("ajax_searchemail.php", {
+                        term: extractLast(request.term)
+                    }, response);
+                },
+                search: function () {
+                    // custom minLength
+                    var term = extractLast(this.value);
+                    if (term.length < 2) {
+                        return false;
+                    }
+                },
+                focus: function () {
+                    // prevent value inserted on focus
+                    return false;
+                },
+                select: function (event, ui) {
+                    var terms = split(this.value);
+                    // remove the current input
+                    terms.pop();
+                    // add the selected item
+                    terms.push(ui.item.value);
+                    // add placeholder to get the comma-and-space at the end
+                    terms.push("");
+                    this.value = terms.join(", ");
+                    return false;
+                }
+            });
+
+
+    }
+
 function do_change() {
     if (xmlhttp.readyState==4) {
         if (xmlhttp.status==200) {
