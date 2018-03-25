0f4c04bc1654c2ea29076a0b60efabe209773b49
Kobi Shmueli <kobi@macron.co.il>
2018-03-25 20:08:43 +0300

    Added a special check for PSEUDO_GROUP.

--- php/karnaf/karnaf_templates.php
+++ php/karnaf/karnaf_templates.php
@@ -1,6 +1,6 @@
 <?php
 ##################################################################
-# Karnaf HelpDesk System - Copyright (C) 2001-2015 Kobi Shmueli. #
+# Karnaf HelpDesk System - Copyright (C) 2001-2018 Kobi Shmueli. #
 # See the LICENSE file for more information.                     #
 ##################################################################
 
@@ -8,7 +8,7 @@ require("../ktools.php");
 check_auth();
 $query = squery("SELECT g.name,t.body FROM (karnaf_templates AS t LEFT JOIN groups AS g ON g.id=t.group_id) WHERE t.id=%d", $_GET['id']);
 while($result = sql_fetch_array($query)) {
-  if(!IsGroupMember($result['name']) && !IsKarnafAdminSession()) continue;
+  if((($result['name'] != PSEUDO_GROUP) || !IsKarnafOperSession()) && !IsGroupMember($result['name']) && !IsKarnafAdminSession()) continue;
   echo $result['body'];
 }
 sql_free_result($query);
