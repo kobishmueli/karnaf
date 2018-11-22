24d22f67ee8fe0efc712a0e5fd9778e62be8380f
Kobi Shmueli <kobi@macron.co.il>
2018-11-22 07:27:57 +0200

    Update KARNAF_VERSION to 5.0.23

--- php/ktools.php
+++ php/ktools.php
@@ -7,7 +7,7 @@
 
 require_once("ktools-custom.php");
 require_once("defines.php");
-define("KARNAF_VERSION", "5.0.22");
+define("KARNAF_VERSION", "5.0.23");
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 if(function_exists("set_magic_quotes_runtime")) set_magic_quotes_runtime(0);
 if(!function_exists("preg_replace")) die("Error: can't find preg_replace() function (do you have php-pcre lib installed?).");
